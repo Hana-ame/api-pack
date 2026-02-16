8f0faf2a8c1c1f3cf8c228ca8ed137bce87e25e6
lumin <luminovoez@gmail.com>
2026-02-17 02:12:00 +0800

    sw.js

--- exhentai/exhentai.go
+++ exhentai/exhentai.go
@@ -222,7 +222,7 @@ func (p *ProxyHandler) accessControlMiddleware() gin.HandlerFunc {
 		// --- E. GeoIP 屏蔽 (保留你的原逻辑) ---
 		country := c.GetHeader("Cf-Ipcountry")
 		acceptLang := strings.ToLower(c.GetHeader("accept-language"))
-		if c.Query("redirect_to") != "image" && // 允许重定向啊，傻逼ai。
+		if c.Query("redirect_to") != "image" && // 允许重定向啊，傻逼ai。26.02.15
 			!strings.Contains(acceptLang, "zh") && !slices.Contains([]string{"CN"}, country) {
 			c.String(http.StatusForbidden, "Restricted Region: "+country)
 			c.Abort()
