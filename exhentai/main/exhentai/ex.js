c25476e12effad286fb963c3386499522f80fc70
Hana Amelia <Luminovoez@outlook.com>
2026-04-10 18:55:33 +0800

    Update ex.js

--- exhentai/main/exhentai/ex.js
+++ exhentai/main/exhentai/ex.js
@@ -704,7 +704,7 @@
   // 6. 右下角悬浮窗
   // =========================================================================
   function initFloatingNotice() {
-    const mark = "0316";
+    const mark = "0410";
     if (localStorage.getItem("iframeClosed") === mark) {
       return;
     }
