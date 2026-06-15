be07df696618c9c85fbeb1a1f5d2d0f1c6e037ef
Hana Amelia <Luminovoez@outlook.com>
2026-06-16 04:33:38 +0800

    Update floating notice mark from '0612' to '0616'

--- exhentai/main/exhentai/ex.js
+++ exhentai/main/exhentai/ex.js
@@ -704,7 +704,7 @@
   // 6. 右下角悬浮窗
   // =========================================================================
   function initFloatingNotice() {
-    const mark = "0612";
+    const mark = "0616";
     if (localStorage.getItem("iframeClosed") === mark) {
       return;
     }
